02cf95400af000eb1e53786cd8555415c5902a4e
aktivgo <aktivgo01062001@gmail.com>
2022-08-26 15:34:35 +0300

    Refactoring

--- go.mod
+++ go.mod
@@ -9,9 +9,15 @@ require (
 	github.com/go-openapi/errors v0.20.2 // indirect
 	github.com/go-openapi/strfmt v0.21.3 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
 	github.com/mitchellh/mapstructure v1.3.3 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
 	go.mongodb.org/mongo-driver v1.10.0 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
 )
 
 require (
@@ -20,5 +26,7 @@ require (
 	github.com/lib/pq v1.10.6
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/semichkin-gopkg/uuid v0.0.1
+	github.com/sirupsen/logrus v1.9.0
+	github.com/x-cray/logrus-prefixed-formatter v0.5.2
 	golang.org/x/tools v0.0.0-20190422233926-fe54fb35175b // indirect
 )
--- internal/domain/user/handler.go
+++ internal/domain/user/handler.go
@@ -7,10 +7,12 @@ import (
 	"io"
 	"net/http"
 	"wire-di/internal/domain"
+	"wire-di/pkg/logger"
 )
 
 type handler struct {
-	svc domain.UserService
+	logger logger.Logger
+	svc    domain.UserService
 }
 
 func (h *handler) FetchByUsername() http.HandlerFunc {
@@ -19,6 +21,7 @@ func (h *handler) FetchByUsername() http.HandlerFunc {
 
 		body, err := io.ReadAll(r.Body)
 		if err != nil {
+			h.logger.Error(err.Error(), nil)
 			w.WriteHeader(http.StatusBadRequest)
 			w.Write([]byte(fmt.Sprintf("{\"error\":\"body read failed: %s\"}", err)))
 			return
@@ -28,6 +31,7 @@ func (h *handler) FetchByUsername() http.HandlerFunc {
 			Username string
 		}
 		if err = json.Unmarshal(body, &params); err != nil {
+			h.logger.Error(err.Error(), nil)
 			w.WriteHeader(http.StatusBadRequest)
 			w.Write([]byte(fmt.Sprintf("{\"error\":\"params unmarshall failed: %s\"}", err)))
 			return
@@ -42,6 +46,7 @@ func (h *handler) FetchByUsername() http.HandlerFunc {
 
 		response, err := json.Marshal(user)
 		if err != nil {
+			h.logger.Error(err.Error(), nil)
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(fmt.Sprintf("{\"error\":\"response marshall failed: %s\"}", err)))
 			return
